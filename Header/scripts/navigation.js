e343c8dc6d7828647bcb00d20a56a554cda056d1
beyersdorfertj <beyersdorfertj@ldschurch.org>
2025-09-08 23:36:55 +0200

    Week1 submission

---
+++ Header/scripts/navigation.js
@@ -0,0 +1,7 @@
+const navBtn = document.querySelector('#ham-btn');
+const navBar = document.querySelector('#nav-bar');
+
+navBtn.addEventListener('click', ()=>{
+    navBtn.classList.toggle('show');
+    navBar.classList.toggle('show');
+})
