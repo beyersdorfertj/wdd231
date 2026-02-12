bd4e3692bbf7a883b90264209e6f672a574a55d9
beyersdorfertj <beyersdorfertj@ldschurch.org>
2026-02-12 21:32:21 +0100

    fix page audit

--- final/scripts/navigation.js
+++ final/scripts/navigation.js
@@ -6,3 +6,7 @@ hamBtn?.addEventListener('click', () => {
     hamBtn.classList.toggle('show');
     navBar.classList.toggle('show');
 });
+
+// Update footer with current year and last modified date
+document.querySelector("#currentYear").textContent = new Date().getFullYear();
+document.querySelector("#lastModified").textContent = `Last Modification: ${document.lastModified}`;
